8a7b523e765aeb6ac0777f55876bd0065cac3bce
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-05-02 18:50:28 +0300

    basic nested entries return logic layout

--- main.ts
+++ main.ts
@@ -2,10 +2,29 @@ import express from 'express';
 import cors from "cors";
 const app = express();
 
+import prismaClient from "./src/setup/orm/prisma";
+
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(201).json({ msg: "Hello World!" });
+  prismaClient.train.findMany({
+    include:{
+      tracker: true
+    }
+  }).then((result) => {
+    let responseData: {}[] = [];
+    if (result) {
+      if (result.length > 0) {
+        responseData = result.map((row) => {
+          const {tracker, ...train} = row;
+          return {...train, trackerID: tracker.id, trackerSerial: tracker.serial};
+        });
+      }
+      res.status(200).json(responseData);
+      return;
+    }
+    res.status(404).json(responseData);
+  });
 })
 
 app.listen(3000, () => console.log("Server is listening on: http:\\\\localhost:3000"));
--- src/setup/orm/prisma.ts
+++ src/setup/orm/prisma.ts
@@ -1,5 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 
-const customClient = new PrismaClient();
+const prismaClient = new PrismaClient();
 
-export default customClient;
+/*prismaClient.train.create({data: {
+  name: 'er-100',
+  tracker: {
+    create: {
+      serial: '68b18d08-de90-4d51-9031-0b8ed1873dfe'
+    }
+  }
+  }}).then(res => {
+    console.log({res})
+}).catch((err) => {
+  console.log({err})
+});*/
+
+export default prismaClient;
