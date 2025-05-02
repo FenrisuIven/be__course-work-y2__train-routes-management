ff5e9b6ad865bc4aaae9d86b16976b8db733b291
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-05-02 21:19:12 +0300

    minor train route getAll func refactor

--- src/routes/train/trainRoutes.ts
+++ src/routes/train/trainRoutes.ts
@@ -3,24 +3,26 @@ import prismaClient from "../../setup/orm/prisma";
 
 import type {Train, Tracker} from "@prisma/client";
 
-const trainRoutes = {
-  getAll: async (req: Request, res: Response) => {
-    const trains: (
-      Train &
-      { tracker: Tracker}
+const getAll = async (req: Request, res: Response) => {
+  const trains: (
+    Train &
+    { tracker: Tracker}
     )[] = await prismaClient.train.findMany({
-      include:{
-        tracker: true
-      }
-    })
+    include:{
+      tracker: true
+    }
+  })
 
-    const responseData: TrainWithTracker [] = trains.map((row) => {
-      const {tracker, ...train} = row;
-      return {...train, trackerID: tracker.id, trackerSerial: tracker.serial};
-    });
+  const responseData: TrainWithTracker[] = trains.map((
+    row: Train & { tracker: Tracker }
+  ): TrainWithTracker => {
+    const {tracker, ...train} = row;
+    return {...train, trackerID: tracker.id, trackerSerial: tracker.serial};
+  });
 
-    res.status(responseData.length > 0 ? 200 : 404).json(responseData);
-  }
+  res.status(responseData.length > 0 ? 200 : 404).json(responseData);
+}
+
+export default {
+  getAll
 };
-
-export default trainRoutes;
