5c2e89be3c4f6bfd7430db97e625038f08453f04
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-05-04 17:15:50 +0300

    update schema: use PostGIS points, remove ArrivalTime

---
+++ src/controllers/trainStop/index.ts
@@ -0,0 +1,27 @@
+// retrieve train stops with their positions in world
+// await prismaClient.$queryRaw`SELECT "name", "stationID", "position"::text FROM "TrainStop"`;
+
+// set train stop position after its creation
+// await prismaClient.$executeRaw`UPDATE "TrainStop" SET "position" = ST_MakePoint(<lon>, <lat>) WHERE "id" = <id>`;
+
+// create new train stop without its position set
+/*prismaClient.trainStop.create({
+  data: {
+    name: 'Train Stop 2',
+    station: {
+      create: {
+        name: 'Station 2',
+        city: "City 2",
+        region: "Region 2",
+        street: "Street 2"
+      }
+    },
+    routes: {
+      create: [
+        {
+          name: "Route 2"
+        }
+      ]
+    }
+  }
+}).then(res => console.log(res)).catch(err => console.log(err));*/
--- src/setup/orm/prisma.ts
+++ src/setup/orm/prisma.ts
@@ -1,18 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../../../prisma/generated/client'
 
 const prismaClient = new PrismaClient();
-
-/*prismaClient.train.create({data: {
-  name: 'er-100',
-  tracker: {
-    create: {
-      serial: '68b18d08-de90-4d51-9031-0b8ed1873dfe'
-    }
-  }
-  }}).then(res => {
-    console.log({res})
-}).catch((err) => {
-  console.log({err})
-});*/
 
 export default prismaClient;
