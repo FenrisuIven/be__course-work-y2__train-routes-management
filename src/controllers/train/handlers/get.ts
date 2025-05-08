84f761182290b407b9dabcce87c141ab72f55594
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-05-09 00:55:13 +0300

    train getAll minor refactor

--- src/controllers/train/handlers/get.ts
+++ src/controllers/train/handlers/get.ts
@@ -1,21 +1,17 @@
-import {NextFunction, Request, Response} from "express";
+import { Request, Response } from "express";
 
 import TrainModel from "../../../models/train";
 
-const getAll = async (req: Request, res: Response, next: NextFunction) => {
-  let include = {};
-  if (req.query.include || Array.isArray(req.query.include)) {
-    include = {
-      voyage: (req.query.include as string[]).includes("voyage"),
-      tracker: (req.query.include as string[]).includes("tracker"),
-    };
-  }
+const getAll = async (req: Request, res: Response) => {
+  const include = (req.query.include as string[]).reduce<Record<string, boolean>>((acc, value) => {
+    return {...acc, [value]: true};
+  }, {});
 
-  const remap = req.query.remap === "" && Object.keys(include).length > 0;
+  const remap = req.query.remap === "" && !Object.keys(include).length;
 
   let responseData = [];
-  if (Object.keys(include).length > 0) {
-    responseData = await TrainModel.GET_ALL_WITH_INCLUDED({include, remap});
+  if (!Object.keys(include).length) {
+    responseData = await TrainModel.GET_ALL_WITH_INCLUDED({ include, remap });
   } else {
     responseData = await TrainModel.GET_ALL();
   }
